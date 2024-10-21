e12a497bbbff791dec9979934fa8025c0c60e0a6
Alberton Francesco <papo.paf2006@gmail.com>
2024-10-21 16:26:35 +0200

    Primo Commit prova

---
+++ Cavallo.java
@@ -0,0 +1,13 @@
+public class Cavallo extends Thread{
+private String nome;
+private int distanza;
+private int fine;
+private static in distmax = 10;
+
+public Cavallo(String nome, int fine){
+    this.nome = nome;
+    this.fine = fine;
+    this.distanza=0;
+}
+
+}
