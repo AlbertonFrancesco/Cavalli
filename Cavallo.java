a66390145c83005ede4aa35131ce8b36f0f96748
Alberton Francesco <papo.paf2006@gmail.com>
2024-10-21 16:41:42 +0200

    Secondo commit

--- Cavallo.java
+++ Cavallo.java
@@ -2,7 +2,7 @@ public class Cavallo extends Thread{
 private String nome;
 private int distanza;
 private int fine;
-private static in distmax = 10;
+private static int distmax = 10;
 
 public Cavallo(String nome, int fine){
     this.nome = nome;
---
+++ Corsa.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Corsa {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci la lunghezza massima in metri: ");
+        int fine= scanner.scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Quanti cavalli vuoi inserie: ");
+        int numCav = nextInt();
+        scanner.nextLine();
+        List<Cavallo> cavalli = new ArreyList<>();
+    }
+}
